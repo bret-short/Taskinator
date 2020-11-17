c7a7dd71c10fc4132bc3cdcdda1c1cc681941ec4
Bret Short <bsshort@LAP1890.local>
2020-11-17 16:25:32 -0700

    added innerHTML DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,24 @@ var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector(".task-list");
 console.log(tasksToDoEl);
 var createTaskHandler = function(event) {
-
+    
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
   
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.classname = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
   };
+  
 formEl.addEventListener("submit", createTaskHandler);
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
