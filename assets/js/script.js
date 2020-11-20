1cd5cbf017d95aeb54eeaa41e12552df74733e7d
Bret Short <bsshort@LAP1890.local>
2020-11-20 11:44:31 -0700

    styles on drag feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,7 +203,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
       event.preventDefault();
-      
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
   };
 
@@ -223,9 +223,21 @@ var dropZoneDragHandler = function(event) {
       else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
       }
+      dropZoneEl.removeAttribute("style");
       dropZoneEl.appendChild(draggableElement);
   };
+
+  var dragLeaveHandler = function(event) {
+      var taskListEl = event.target.closest(".task-list");
+      if (taskListEl) {
+          taskListEl.removeAttribute("style");
+      }
+  }
+
+pageContentEl.addEventListener("dragstart", dragTaskHandler);  
 
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
